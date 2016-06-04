a8b233aded473baa4add04df0e48b4484475f8f2
techraiders <hello_navneet@hotmail.com>
2016-06-05 02:58:50 +0530

    Project submitted to University of Pune

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ecommerce/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/ecommerce/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ecommerce/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/ecommerce/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/ecommerce/upload/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/ecommerce/upload/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/ecommerce/upload/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/ecommerce/upload/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/ecommerce/upload/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/ecommerce/upload/image/');
+define('DIR_CACHE', 'C:/wamp/www/ecommerce/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/ecommerce/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/ecommerce/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/ecommerce/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/ecommerce/upload/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wamp/www/ecommerce/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'Navneet');
+define('DB_PASSWORD', 'secret');
+define('DB_DATABASE', 'bestdealdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
